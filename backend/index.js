8ae50500ef85f0ac18c5d1327bd8a94f177d211d
Guilherme Fernandes <57098649+guilfer-dev@users.noreply.github.com>
2021-10-18 18:01:02 -0300

    [feat] Upload
    Upload of repository once stored offline

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require('cors');
+const mongoose = require("mongoose");
+const routes = require('./routes');
+require("dotenv").config();
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_AUTH).then(console.log("Connected to DB!"))
+
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+
+app.listen(3333)
