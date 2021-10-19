6a43c889e54ad7c5553d0de5c6a3d303197a4adb
Guilherme Fernandes <57098649+guilfer-dev@users.noreply.github.com>
2021-10-19 12:41:36 -0300

    [feat] add cors env

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,9 @@ const app = express();
 mongoose.connect(process.env.MONGO_AUTH).then(console.log("Connected to DB!"))
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+}));
 app.use(routes);
 
 app.listen(process.env.PORT || 3333)
